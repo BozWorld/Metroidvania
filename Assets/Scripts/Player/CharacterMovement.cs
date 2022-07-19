48b43ed9142c0241a712efbe6cc9989561f055a8
Moeva <mondohamoeva@hotmail.fr>
2022-07-19 20:00:50 +0200

    {{ add first collision }}
    -> Ajout d'un premier système de collision.
    -> il est limité par quelque soucis technique ( jusqu'à un certains bord il ne reconnais plus )
    -> Le saut se fait uniquement quand on retouche le sol

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask _groundLayerMask;
     public float Speed;
     private PlayerInput _input;
     private Vector2 _rawMovement;
-    private float m_JumpForce;
+
     [Range(1,10)]
     public float JumpVelocity;
     private bool _isJumping;
     public float FallMultiplier = 2.5f;
     public float lowJumpMultiplier = .2f;
     private Rigidbody2D _rb;
+    public bool canJump;
 
+    public BoxCollider2D ground;
     private void Awake(){
         _rb = GetComponent<Rigidbody2D>();
         _input = new PlayerInput();
@@ -28,6 +31,7 @@ public class CharacterMovement : MonoBehaviour
     private void FixedUpdate () {
         Move();
         FallModifier();
+        IsGrounded();
     }
 
     private void Move(){
@@ -35,8 +39,11 @@ public class CharacterMovement : MonoBehaviour
     }
 
     private void Jump(){
-        _isJumping = true;
-        GetComponent<Rigidbody2D>().velocity = Vector2.up * JumpVelocity;
+        if(IsGrounded())
+            {
+                _isJumping = true;
+                GetComponent<Rigidbody2D>().velocity = Vector2.up * JumpVelocity;
+            }
     }
 
     private void FallModifier(){
@@ -46,6 +53,20 @@ public class CharacterMovement : MonoBehaviour
         else if (_rb.velocity.y > 0  && !_isJumping ){
             _rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
+    }
+
+    public bool IsGrounded(){
+        float extraHeightText = .01f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(ground.bounds.center, Vector2.down, ground.bounds.extents.y, _groundLayerMask);
+        raycastHit = Physics2D.OverlapBox()
+        if(raycastHit.collider != null){
+            rayColor = Color.green;
+        } else {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(ground.bounds.center, Vector2.down * (ground.bounds.extents.y + extraHeightText), rayColor);
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
     }
 
 
