0fe068f67e58a4c0a8102a07f53546118ce57b94
Moeva <mondohamoeva@hotmail.fr>
2022-07-21 15:48:25 +0200

    {{ changement du système d'animation et ajout du dash fonctionnel }}
    -> ajout d'un script gerant les animation par le code , krep ajoute dans l'animator les anim et moi je les traites a la main
    -> Update du prefab player
    -> début du système de dash
    -> Les animations sont maintenant toutes fonctionnelles et facilement itérable
    -> début de jeu des stats du player il faudra revoir mais tout est la ils suffit de bidouiller
    -> quelque travaux d'optimisation mais léger il faudra prendre un peu de temps pour que je relis mon code et rajoute quelque action
    -> pour l'instant le dash est un dash il faut maintenant implémenter tout les système lié a l'attaque 1
    -> changement du système de groundcheck le précédent était beuger et provoquer des problème

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private CharacterMovement _player;
+    [SerializeField] private Animator _anim;
+    [SerializeField] private SpriteRenderer _renderer;
+
+    private float _lockedTill;
+    private bool _isJumping;
+    private bool _isFalling;
+    private bool _isMoving;
+    private bool _isGrounded;
+
+    private void Start()
+    {
+        _player.Input.Player.Move.started += ctx => _isMoving = true;
+        _player.Input.Player.Move.started += ctx => _isJumping = true;
+    }
+
+    private void Update()
+    {
+        _isGrounded = _player.IsGrounded();
+        var state = GetState();
+
+        _isJumping = false;
+        _isFalling = false;
+
+        if (state == _currentState) return;
+        _anim.CrossFade(state, 0.2f, 0);
+        _currentState = state;
+
+    }
+    private int GetState() 
+    {
+        if (Time.time < _lockedTill) return _currentState;
+        // Priorities
+        if (_isJumping) return Jump;
+        if (_isGrounded) return _player.RawMovement.x == 0 ? Idle : Walk;
+        return _player.Rb.velocity.y > 0 ? Jump : Fall;
+
+        int LockState(int s, float t) {
+            _lockedTill = Time.time + t;
+            return s;
+        }
+    }
+
+    #region hashedProprieties
+
+    private int _currentState;
+    private static readonly int Idle = Animator.StringToHash("Idle");
+    private static readonly int Walk = Animator.StringToHash("Walk");
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int Fall = Animator.StringToHash("Fall");
+
+    #endregion
+}
--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -6,17 +6,18 @@ public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask _groundLayerMask;
     public float Speed;
-    private PlayerInput _input;
-    private Vector2 _rawMovement;
-    private SpriteRenderer playerSprite;
+    public PlayerInput Input;
+    public Vector2 RawMovement;
+    private float _checkDirection;
+    public SpriteRenderer PlayerSprite;
 
     [Range(1,10)]
     public float JumpVelocity;
     private bool _isJumping;
     public float FallMultiplier = 2.5f;
     public float LowJumpMultiplier = .2f;
-    private Rigidbody2D _rb;
-    public bool CanJump;
+    [HideInInspector] public Rigidbody2D Rb;
+    private bool _canJump;
     public BoxCollider2D Ground;
     private float _yVelocity;
     public Animator PlayerAnimator;
@@ -24,20 +25,20 @@ public class CharacterMovement : MonoBehaviour
     public float DashDistance;
     public float DashSpeed;
     public float DashinCoolDown;
-    private bool _canDash;
+    private bool _canDash = true;
     private bool _isDashing;
     public TrailRenderer DashTrail;
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _input = new PlayerInput();
-        _input.Player.Enable();
-        _input.Player.Move.started += ctx => _rawMovement = ctx.ReadValue<Vector2> ();
-        _input.Player.Move.canceled += _ => _rawMovement = Vector2.zero;
-        _input.Player.Jump.started += ctx => Jump();
-        _input.Player.Jump.canceled += ctx => _isJumping = false;
-        _input.Player.Dash.started += ctx => Dash();
+        Rb = GetComponent<Rigidbody2D>();
+        Input = new PlayerInput();
+        Input.Player.Enable();
+        Input.Player.Move.started += ctx => RawMovement = ctx.ReadValue<Vector2> ();
+        Input.Player.Move.canceled += _ => RawMovement = Vector2.zero;
+        Input.Player.Jump.started += ctx => Jump();
+        Input.Player.Jump.canceled += ctx => _isJumping = false;
+        Input.Player.Dash.started += ctx => StartCoroutine(Dash());
     }
 
     private void FixedUpdate() 
@@ -49,60 +50,54 @@ public class CharacterMovement : MonoBehaviour
 
     private void Move()
     {
-        if(_rawMovement != Vector2.zero){
-            PlayerAnimator.SetBool("isMoving", true);
-            transform.position += (Vector3)_rawMovement * Speed * Time.deltaTime;
-        }
-        else
-        {
-            PlayerAnimator.SetBool("isMoving", false);
+        if(RawMovement != Vector2.zero){
+            PlayerSprite.flipX = RawMovement.x < 0;
+            transform.position += (Vector3)RawMovement * Speed * Time.deltaTime;
         }
     }
 
-    private void Jump()
+    public void Jump()
     {
-        if (IsGrounded())
+        if (IsGrounded() && !_isJumping)
         {
             _isJumping = true;
-            _rb.velocity = Vector2.up * JumpVelocity;
-            
+            Rb.velocity = Vector2.up * JumpVelocity;
         }
     }
 
     private void FallModifier()
     {
-        if (_rb.velocity.y < 0)
+        if (Rb.velocity.y < 0)
         {
-            _rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
+            Rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
         }
-        else if (_rb.velocity.y > 0  && !_isJumping )
+        else if (Rb.velocity.y > 0  && !_isJumping )
         {
-            _rb.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
+            Rb.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 
     public bool IsGrounded()
     {
-        _yVelocity = Mathf.Clamp(_rb.velocity.y, -1, 1);
+        _yVelocity = Mathf.Clamp(Rb.velocity.y, -1, 1);
         PlayerAnimator.SetFloat("yVelocity", _yVelocity);
         
-        float extraHeightText = .01f;
-        RaycastHit2D raycastHit = Physics2D.Raycast(Ground.bounds.center, Vector2.down, Ground.bounds.extents.y, _groundLayerMask);
+        float extraHeightText = 0.5f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(Ground.bounds.center, Ground.bounds.size,0f, Vector2.down ,extraHeightText, _groundLayerMask);
         Color rayColor;
         
         if(raycastHit.collider != null)
         {
             rayColor = Color.green;
-            PlayerAnimator.SetBool("isJumping", false);
         } 
         else 
-        {
-            PlayerAnimator.SetBool("isJumping", true);
+        {;
             rayColor = Color.red;
         }
         
-        Debug.DrawRay(Ground.bounds.center, Vector2.down * (Ground.bounds.extents.y + extraHeightText), rayColor);
-        Debug.Log(raycastHit.collider);
+        Debug.DrawRay(Ground.bounds.center + new Vector3(Ground.bounds.extents.x,0), Vector2.down * ( Ground.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(Ground.bounds.center - new Vector3(Ground.bounds.extents.x,0), Vector2.down * ( Ground.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(Ground.bounds.center - new Vector3(Ground.bounds.extents.x, Ground.bounds.extents.y +  extraHeightText), Vector2.right * ( Ground.bounds.extents.x), rayColor);
         return raycastHit.collider != null;
     }
 
@@ -110,20 +105,36 @@ public class CharacterMovement : MonoBehaviour
     
     private IEnumerator Dash()
     {
-        Debug.Log("Dashing");
-        float originaGravity = _rb.gravityScale;
-        _rb.gravityScale = 0;
-        _rb.velocity = new Vector2(transform.localScale.x * DashDistance, 0f);
-        DashTrail.emitting = true;
-        yield return new WaitForSeconds(DashSpeed);
-        DashTrail.emitting = false;
-        _rb.gravityScale = originaGravity;
-        _isDashing = false;
-        yield return new WaitForSeconds(DashinCoolDown);
-        _canDash = true;
-        // Vector2 dashPos = new Vector2(transform.position.x + DashDistance, transform.position.y);
-        // transform.position = Vector2.Lerp(transform.position,dashPos, DashSpeed);
-        //_rb.velocity = new Vector2(transform.position.x * DashDistance *, transform.position.y);
+        if( _canDash)
+        {
+            _canDash = false;
+            _isDashing = true;
+            float originaGravity = Rb.gravityScale;
+            Rb.gravityScale = 0;
+            Rb.velocity = new Vector2((transform.localScale.x * DashDistance) * _checkDirection , 0f);
+            DashTrail.emitting = true;
+            yield return new WaitForSeconds(DashSpeed);
+            DashTrail.emitting = false;
+            Rb.gravityScale = originaGravity;
+            _isDashing = false;
+            yield return new WaitForSeconds(DashinCoolDown);
+            _canDash = true;
+            // Vector2 dashPos = new Vector2(transform.position.x + DashDistance, transform.position.y);
+            // transform.position = Vector2.Lerp(transform.position,dashPos, DashSpeed);
+            //Rb.velocity = new Vector2(transform.position.x * DashDistance *, transform.position.y);
+        }
+    }
+
+    private void flip()
+    {
+        if(RawMovement.x < 0)
+        {
+            _checkDirection = -1;
+        }
+        else
+        {
+            _checkDirection = 1;
+        }
     }
 
     #endregion
